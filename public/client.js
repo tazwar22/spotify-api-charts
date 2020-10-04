3f147d833ac845a4e618a30320f86f3b97de24ef
Ahnaf Tazwar Ayub <tazwarahnaf22@gmail.com>
2020-10-04 00:23:10 -0700

    Started plotting histogram; simple Valence histogram

--- public/client.js
+++ public/client.js
@@ -259,7 +259,7 @@ $('#short_term_audio').on('click',function(){
 
         console.log("Getting Audio Stats...")
         $.ajax({
-            url: "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=10",
+            url: "https://api.spotify.com/v1/me/top/tracks?time_range=short_term&limit=50",
             type: "GET",
             dataType: "json",
             beforeSend: function(xhr) {
@@ -327,59 +327,61 @@ $('#short_term_audio').on('click',function(){
 function plotAudioHist(data){
 
 
-    // set the dimensions and margins of the graph
-var margin = {top: 10, right: 30, bottom: 30, left: 40},
-width = 460 - margin.left - margin.right,
-height = 400 - margin.top - margin.bottom;
+        // set the dimensions and margins of the graph
+    var margin = {top: 10, right: 30, bottom: 30, left: 40},
+    width = 460 - margin.left - margin.right,
+    height = 400 - margin.top - margin.bottom;
 
-// append the svg object to the body of the page
-var svg = d3.select("#audio_hist")
-.append("svg")
-.attr("width", width + margin.left + margin.right)
-.attr("height", height + margin.top + margin.bottom)
-.append("g")
-.attr("transform",
-      "translate(" + margin.left + "," + margin.top + ")");
+    // append the svg object to the body of the page
+    var svg = d3.select("#audio_hist")
+    .append("svg")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("transform",
+        "translate(" + margin.left + "," + margin.top + ")");
 
 
-// X axis: scale and draw:
-var x = d3.scaleLinear()
-  .domain([0, d3.max(data, function(d) { 
-    console.log(d)  
-    return +d})])     // can use this instead of 1000 to have the max of data: 
-  .range([0, width]);
-svg.append("g")
-  .attr("transform", "translate(0," + height + ")")
-  .call(d3.axisBottom(x));
+    // X axis: scale and draw:
+    var x = d3.scaleLinear()
+                .domain([0, d3.max(data, function(d) {return +d})])    
+                .range([0, width]);
+    svg
+        .append("g")
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(x));
 
-// set the parameters for the histogram
-var histogram = d3.histogram()
-  .value(function(d) { return d; })   // I need to give the vector of value
-  .domain(x.domain())  // then the domain of the graphic
-  .thresholds(x.ticks(5)); // then the numbers of bins
+    // set the parameters for the histogram
+    var histogram = d3.histogram()
+                        .value(function(d) { return d; })   // I need to give the vector of value
+                        .domain(x.domain())  // then the domain of the graphic
+                        .thresholds(x.ticks(10)); // then the numbers of bins
 
-// And apply this function to data to get the bins
-console.log(data)
-var bins = histogram(data);
-console.log(bins)
+    // And apply this function to data to get the bins
+    var bins = histogram(data);
+    console.log(bins)
 
-// Y axis: scale and draw:
-var y = d3.scaleLinear()
-  .range([height, 0]);
-  y.domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
-svg.append("g")
-  .call(d3.axisLeft(y));
+    // Y axis: scale and draw:
+    var y = d3.scaleLinear()
+              .range([height, 0]);
 
-// append the bar rectangles to the svg element
-svg.selectAll("rect")
-  .data(bins)
-  .enter()
-  .append("rect")
-    .attr("x", 1)
-    .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
-    .attr("width", function(d) { return 2; })
-    .attr("height", function(d) { return height - y(d.length); })
-    .style("fill", "#69b3a2")
+    y
+    .domain([0, d3.max(bins, function(d) { return d.length; })]);   // d3.hist has to be called before the Y axis obviously
+
+
+    svg.append("g")
+        .call(d3.axisLeft(y));
+
+    // append the bar rectangles to the svg element
+    svg.selectAll("rect")
+        .data(bins)
+        .enter()
+        .append("rect")
+            .attr("x", 1)
+            .attr("transform", function(d) { return "translate(" + x(d.x0) + "," + y(d.length) + ")"; })
+            .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
+            .attr("height", function(d) { return height - y(d.length); })
+            .style("fill", "#69b3a2")
 
 }
 
